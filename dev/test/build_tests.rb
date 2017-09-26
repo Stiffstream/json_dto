189a392c469fe89b54a151ed94c9d1c8e551f45a
Yauheni Akhotnikau <eao197@gmail.com>
2017-09-26 08:38:20 +0300

    Unit test for checking std::vector<bool>.

--- dev/test/build_tests.rb
+++ dev/test/build_tests.rb
@@ -7,6 +7,7 @@ MxxRu::Cpp::composite_target {
 	required_prj( "test/floats/prj.ut.rb" )
 	required_prj( "test/optional/prj.ut.rb" )
 	required_prj( "test/std_optional/prj.ut.rb" )
+	required_prj( "test/std_vector_bool/prj.ut.rb" )
 	required_prj( "test/nullable/prj.ut.rb" )
 	required_prj( "test/non_intrusive_io/prj.ut.rb" )
 	required_prj( "test/user_defined_io/prj.ut.rb" )
---
+++ dev/test/std_vector_bool/main.cpp
@@ -0,0 +1,44 @@
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
+
+#include <json_dto/pub.hpp>
+
+struct data_t
+{
+	bool m_single;
+	std::vector<bool> m_multi;
+
+	template<typename I>
+	void json_io(I & io)
+	{
+		io & json_dto::mandatory("single", m_single)
+			& json_dto::mandatory("multi", m_multi);
+	}
+};
+
+TEST_CASE( "read from json" , "read" )
+{
+	{
+		const std::string json_data{
+			R"JSON(
+			{"single":true, "multi":[false, true]}
+			)JSON" };
+		auto obj = json_dto::from_json< data_t >( json_data );
+
+		REQUIRE( obj.m_single );
+		REQUIRE( 2 == obj.m_multi.size() );
+		REQUIRE( !obj.m_multi[0] );
+		REQUIRE( obj.m_multi[1] );
+	}
+}
+
+TEST_CASE( "write to json" , "write" )
+{
+	{
+		data_t obj{ true, {true, false, true}};
+		const auto r = json_dto::to_json( obj );
+
+		REQUIRE( R"({"single":true,"multi":[true,false,true]})" == r );
+	}
+}
+
